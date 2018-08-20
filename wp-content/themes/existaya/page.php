8a1b2c2dde683d8d570a4c7e6261b916b9b54eae
Daniel M. Sanchez <dmsanchez86@misena.edu.co>
2018-08-19 23:01:22 -0500

    se agrego archivo para las paginas por defecto

---
+++ wp-content/themes/existaya/page.php
@@ -0,0 +1,12 @@
+<?php
+
+get_header(); ?>
+
+<div class="app-page">
+	<div class="app-page-title text-center">
+		<h1><i class="fas fa-file"></i> <?= the_title() ?></h1>
+	</div>
+</div>
+
+<?php
+get_footer();
