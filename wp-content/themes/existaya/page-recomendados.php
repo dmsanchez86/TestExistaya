bf3a0c78404241fa5c404b12a5467bd49060d9d1
Daniel M. Sanchez <dmsanchez86@misena.edu.co>
2018-08-19 23:01:40 -0500

    se agrego template solo para la pagina de recomendados

---
+++ wp-content/themes/existaya/page-recomendados.php
@@ -0,0 +1,13 @@
+<?php
+/* Template Name: Pagina de Recomendados */
+
+get_header(); ?>
+
+<div class="app-page">
+	<div class="app-page-title text-center">
+		<h1><i class="fas fa-images"></i> <?= the_title() ?></h1>
+	</div>
+</div>
+
+<?php
+get_footer();
