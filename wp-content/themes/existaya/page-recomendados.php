9b471d00aa1f162c0c6384c7ec06b2783fc7b08e
Daniel M. Sanchez <dmsanchez86@misena.edu.co>
2018-08-20 00:55:51 -0500

    se crearon articulos base en la pagina de recomendados

--- wp-content/themes/existaya/page-recomendados.php
+++ wp-content/themes/existaya/page-recomendados.php
@@ -7,6 +7,85 @@ get_header(); ?>
 	<div class="app-page-title text-center">
 		<h1><i class="fas fa-images"></i> <?= the_title() ?></h1>
 	</div>
+
+	<div class="content-articles">
+		<div class="container">
+			<div class="content-articles-row">
+				<div class="content-articles-item">
+					<div class="row">
+						<div class="col-7">
+							<div class="image-book">
+								<img src="<?= get_stylesheet_directory_uri(); ?>/src/images/plantilla.png" alt="">
+							</div>
+						</div>
+						<div class="col">
+							<div class="description-book">
+								<div class="row align-items-center">
+									<div>
+										<h6>La literatura une a todos los generos</h6>
+										<h1>20% de descuento <br>en tienda virtual</h1>
+										<div><span><i class="fas fa-tag"></i> aplica para libros fisicos de la tienda virtual</span></div>
+										<div><span><i class="fas fa-calendar"></i> Marzo 12 de 2018 | 00:00 AM - Marzo 12 de 2018 | 11:59 PM</span></div>
+										<div>
+											<button>Leer más información</button>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="content-articles-item">
+					<div class="row">
+						<div class="col-7">
+							<div class="image-book">
+								<img src="<?= get_stylesheet_directory_uri(); ?>/src/images/plantilla.png" alt="">
+							</div>
+						</div>
+						<div class="col">
+							<div class="description-book">
+								<div class="row align-items-center">
+									<div>
+										<h6>La literatura une a todos los generos</h6>
+										<h1>20% de descuento <br>en tienda virtual</h1>
+										<div><span><i class="fas fa-tag"></i> aplica para libros fisicos de la tienda virtual</span></div>
+										<div><span><i class="fas fa-calendar"></i> Marzo 12 de 2018 | 00:00 AM - Marzo 12 de 2018 | 11:59 PM</span></div>
+										<div>
+											<button>Leer más información</button>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="content-articles-item">
+					<div class="row">
+						<div class="col-7">
+							<div class="image-book">
+								<img src="<?= get_stylesheet_directory_uri(); ?>/src/images/plantilla.png" alt="">
+							</div>
+						</div>
+						<div class="col">
+							<div class="description-book">
+								<div class="row align-items-center">
+									<div>
+										<h6>La literatura une a todos los generos</h6>
+										<h1>20% de descuento <br>en tienda virtual</h1>
+										<div><span><i class="fas fa-tag"></i> aplica para libros fisicos de la tienda virtual</span></div>
+										<div><span><i class="fas fa-calendar"></i> Marzo 12 de 2018 | 00:00 AM - Marzo 12 de 2018 | 11:59 PM</span></div>
+										<div>
+											<button>Leer más información</button>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
 
 <?php
